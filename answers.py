62e9ccb877adfbc391fcb0f4ead8eb367fca601a
priya <priya@localhost.localdomain>
2021-07-27 05:22:40 -0600

    added another way for Q1

--- answers.py
+++ answers.py
@@ -1,13 +1,26 @@
-# Answers to the Data Exercise
+# ------------Answers to the Data Exercise---------------
 
 
-# PREPARATIONS: 
-# Add a new unique student record to the student table, notating and showing your work in the file created above.
-insert into student (id, first_name, last_name, dob) values (7, 'Priyanka', 'Goyal', '1990-05-07');
+##### PREPARATIONS: 
+### Add a new unique student record to the student table, notating and showing your work in the file created above.
+INSERT INTO student (id, first_name, last_name, dob) VALUES (7, 'Priyanka', 'Goyal', '1990-05-07');
 
 
-# QUESTIONS:
-# What students by first name, last name, and major name sorted alphabetically by last name have majors from the Engineering or Language Arts departments?
+##### QUESTIONS:
+### What students by first name, last name, and major name sorted alphabetically by last name have majors from the Engineering or Language Arts departments?
+select s.first_name, s.last_name, major_name 
+from student_major sm 
+inner join 
+(select m.id as maj_id, m.major_name from major m 
+INNER JOIN
+(SELECT d.id as depart_id from department d where d.department_name in ('Engineering', 'Language Arts'))
+on m.department_id = depart_id)
+on sm.major_id = maj_id
+INNER JOIN
+student s on s.id = sm.student_id
+ORDER BY s.last_name asc;
+
+# Another way
 select s.first_name, s.last_name, TEMP.major_name 
 from student_major sm 
 inner join 
@@ -16,28 +29,28 @@ SELECT d.id as depart_id from department d where d.department_name in ('Engineer
 on sm.major_id = TEMP.maj_id
 INNER JOIN
 student s on s.id = sm.student_id
-order by s.last_name asc;
+ORDER BY s.last_name asc;
 
 
-# How many students are there per major and major department?
+### How many students are there per major and major department?
 # count of students per major
 select m.id, m.major_name, count(sm.major_id) as student_major_count 
 from major m
-left join student_major sm 
+LEFT JOIN student_major sm 
 on m.id = sm.major_id
-group by m.id;
+GROUP BY m.id;
 
 # Count of students per major department
-Select d.id, d.department_name, SUM(major_count) as student_dept_count
+Select d.id, d.department_name, SUM(student_major_count) as student_dept_count
 from department d 
 LEFT JOIN 
 (select major.department_id as depart_id, count(sm.major_id) as student_major_count 
  from major
 LEFT JOIN student_major sm 
  on major.id = sm.major_id
-group by major.id) 
+GROUP BY major.id) 
 on d.id = depart_id
-group by d.id;
+GROUP BY d.id;
 
 
-# Exporting Dataset to csv
+### Exporting Dataset to csv
