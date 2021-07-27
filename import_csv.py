4509d88a2cacea7e3cd6f754dfef19e9d166cfb7
priya <priya@localhost.localdomain>
2021-07-27 11:55:03 -0600

    updated answers with checks

--- import_csv.py
+++ import_csv.py
@@ -8,16 +8,60 @@ dirpath = os.getcwd()
 DB_CONN = dirpath + "/student_major.db"
 INPUT_CSV = dirpath + "/question_three.csv"
 
-# If file exists and ends with .csv extension, read it else raise an error
+def check_if_csv_valid(df):
+    # Check if dataframe is empty
+    if df.empty:
+        print("Empty file. Finishing execution")
+        return False
+
+    # Check for nulls
+    if df.isnull().values.any():
+        raise Exception("Null values found")
+
+    # Primary Key Check
+    # Won't work here as one student is taking multiple courses
+    # Won't work here as one course is been taken by multiple students
+    # if df['pk'].is_unique:
+    #    pass
+    # else:
+    #    raise Exception("Primary Key check is violated")
+    return True
+
+
 if INPUT_CSV.endswith('.csv'):
-    csv_df = pd.read_csv(INPUT_CSV, sep=',')
+    # To read only student names column separated by a comma from the input file
+    csv_df = pd.read_csv(INPUT_CSV, sep=',', usecols=['first_name', 'last_name'])
 else:
     raise FileNotFoundError("No CSV file found, check location.")
 
 try:
     # Connect to database
     conn = sqlite3.connect(DB_CONN)
-    # write to the SQLite DB, first create the table
+
+    # Check if CSV Dataframe is valid
+    if check_if_csv_valid(csv_df):
+        pass
+    else:
+        raise Exception("Invalid input CSV file.")
+
+    # write to the SQLite DB in the student table
+    # From Pandas Docs:
+    # if_exists{‘fail’, ‘replace’, ‘append’}, default ‘fail’
+    #
+    # How to behave if the table already exists.
+    #
+    #         fail: Raise a ValueError.
+    #
+    #         replace: Drop the table before inserting new values.
+    #
+    #         append: Insert new values to the existing table.
+    #
+    # I wanted to use student table but it was messing up the original table
+    # fail, replace and append all three options are giving undesirable results with 'student' table
+    # It's adding null values to the blank columns
+    # I wanted to say that if record exists in the table student then pass else insert
+    # Pandas does not have 'UPSERT' functionality
+    # I am solely creating a separate table to store students name
     csv_df.to_sql("MyTable", conn, if_exists='replace', index=False)
 
 # Print error, if any
@@ -27,3 +71,4 @@ except sqlite3.Error as e:
 # Close database connection
 finally:
     conn.close()
+
--- readme.md
+++ readme.md
@@ -22,3 +22,14 @@ Project readme file created.
 2. Answer written using Left Joins, aggregate functions like COUNT(), SUM() and using GROUP BY. 
 3. Export CSV file from the database using Pandas. Use ```export_csv.py``` to run the Python script separately.
 4. Import CSV file into the database using Pandas. Use ```import_csv.py``` to run the Python script separately.
+
+For Question4: I uploaded the students names in a new table than the already existing students table as
+it was creating integrity violation. I was looking for 'UPSERT' type of functionality but in Pandas it only
+exists at the Table level, not at the records level. I wanted to add a check that if the name exist in table student then
+pass else insert student name. But with the incoming data from .csv file makes it hard to create a testcase. 
+Here, validating would be making sure the student names are available before trying to UPDATE that change. 
+
+If I was using students table, then the whole table was overfilling with repeatitive values, getting dropped or raising ValueError. 
+If I had used the combination of Student, major then update won't work because it would update all of the students having multiple subjects.
+
+I would be open to discuss the solution for this question. Thank you. 
